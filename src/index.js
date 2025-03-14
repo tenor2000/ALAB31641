7554b2395dd468ad12f4d2e16e0cba977a0946b5
tenor2000 <tenor2000@gmail.com>
2025-03-14 15:07:13 -0400

    registration validation working

--- src/index.js
+++ src/index.js
@@ -3,44 +3,88 @@ const errorDisplay = document.getElementById("errorDisplay");
 const registrationForm = document.getElementById("registration");
 const loginForm = document.getElementById("login");
 
-function validateForm(target) {
-  const userName = target.username;
-  const userEmail = target.email;
-  const userPassword = target.password;
-  const passwordCheck = target.passwordCheck;
-  const terms = target.terms;
+registrationForm.addEventListener("submit", validateForm);
+loginForm.addEventListener("submit", validateForm);
+
+function validateForm(e) {
+  const userName = e.target.username.value;
+  const userEmail = e.target.email.value;
+  const userPassword = e.target.password.value;
+  const passwordCheck = e.target.passwordCheck.value;
+  const terms = e.target.terms.checked;
+
+  // console.log(e.target);
 
   if (!validateUsername(userName)) {
+    e.preventDefault();
     displayMessage("Username is invalid.");
     return false;
   } else if (!validateEmail(userEmail)) {
+    e.preventDefault();
     displayMessage("Email is invalid.");
     return false;
   } else if (!validatePassword(userPassword)) {
+    e.preventDefault();
     displayMessage("Password is invalid.");
     return false;
   } else if (!validatePasswordMatch(userPassword, passwordCheck)) {
+    e.preventDefault();
     displayMessage("Passwords do not match.");
     return false;
   } else if (!validateTerms(terms)) {
+    e.preventDefault();
     displayMessage("You must agree to terms.");
     return false;
   }
 
   return true;
 
-  function validateUsername(name) {}
+  function validateUsername(name) {
+    // ^ pattern enforced from beginning of string
+    // [a-zA-Z0-9] any one of these characters are valid
+    // {4,} at least 4 character long
+    // $ pattern enforced until the end of the string
+    const regex = /^[a-zA-Z0-9]{4,}$/;
+    if (name === "") return false;
 
-  function validateEmail(email) {}
+    return regex.test(name);
+  }
 
-  function validatePassword(password) {}
+  function validateEmail(email) {
+    // ^ pattern enforced from beginning of string
+    // [a-zA-Z0-9._%+-] at least 1 a-z but may be more
+    // + at least 1 or more, same as {1,}
+    // $ pattern enforced until the end of the string
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (email === "") return false;
 
-  function validatePasswordMatch(p1, p2) {}
+    return regex.test(email);
+  }
 
-  function validateTerms(terms) {}
+  function validatePassword(password) {
+    // ^ pattern enforced from beginning of string
+    // (?=.*[a-z]) at least 1 a-z but may be more
+    // (?=.*[A-Z]) at least 1 A-Z but may be more
+    // (?=.*\d) at least 1 digit but may be more
+    // [a-zA-Z0-9] any one of these chars
+    // {12,} at least 12 character long but may be longer
+    // $ pattern is enforced until the end of the string
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-z0-9]{12,}$/;
+
+    return regex.test(password);
+  }
+
+  function validatePasswordMatch(p1, p2) {
+    return p1 === p2;
+  }
+
+  function validateTerms(terms) {
+    return terms;
+  }
 
   function displayMessage(text) {
     clearContainer(errorDisplay);
+    errorDisplay.style.display = "block";
     const message = newEl("p");
     message.textContent = text;
     message.style.color = "red";
@@ -49,7 +93,6 @@ function validateForm(target) {
 }
 
 function clearContainer(container) {
-  while (container.firstChild) {
-    container.remove(container.firstChild);
-  }
+  console.log("clear");
+  container.innerHTML = "";
 }
